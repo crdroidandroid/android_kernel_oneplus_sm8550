75124b2fe74a654a8bdca3ba109e09bbc9452817
Miguel de Dios <migueldedios@google.com>
2021-07-06 14:34:48 +0900

    drivers: iommu: Specify sync probe for qsmmuv500-tbu
    Bug: 115776306
    Bug: 77146523
    
    Signed-off-by: Miguel de Dios <migueldedios@google.com>
    Change-Id: Iff99e30c8a11d4912801e543d9644544113d4ac9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -4211,6 +4211,7 @@ static struct platform_driver arm_smmu_driver = {
 		.of_match_table		= arm_smmu_of_match,
 		.pm			= &arm_smmu_pm_ops,
 		.suppress_bind_attrs    = true,
+		.probe_type	= PROBE_FORCE_SYNCHRONOUS,
 	},
 	.probe	= arm_smmu_device_probe,
 	.remove	= arm_smmu_device_remove,
