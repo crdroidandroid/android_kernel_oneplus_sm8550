fec384af9ec40a6fca6733970275432a72e7fbe9
Zhenhua Huang <quic_zhenhuah@quicinc.com>
2023-04-24 18:46:02 +0800

    iommu/arm-smmu: add qcom smmuv2 support
    Add qcom smmuv2 support.
    The single change combined with below on msm-5.4:
    commit 5f1ea1aa0cf1("iommu/arm-smmu: update debug logs for qsmmuv2 sync
    timeout")
    commit 8980e1419468("iommu/arm-smmu: Update the implementation defined page
    number macros")
    commit 3f127ee7e14e("iommu/arm-smmu: add qsmmuv2 sync timeout handler")
    commit 9aa051814296("iommu/arm-smmu: add qcom smmuv2 support").
    
    Change-Id: I9d02d24a5f55fcb4d6cca3cec3d135724dc0c98b
    Signed-off-by: Zhenhua Huang <quic_zhenhuah@quicinc.com>
    Signed-off-by: Srinivasarao Pathipati <quic_c_spathi@quicinc.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.h
+++ drivers/iommu/arm/arm-smmu/arm-smmu.h
@@ -327,11 +327,6 @@ enum arm_smmu_implementation {
 	QCOM_SMMUV500,
 };
 
-struct arm_smmu_impl_def_reg {
-	u32 offset;
-	u32 value;
-};
-
 /*
  * Describes resources required for on/off power operation.
  * Separate reference count is provided for atomic/nonatomic
@@ -710,6 +705,8 @@ static inline void arm_smmu_writeq(struct arm_smmu_device *smmu, int page,
  * Implementation defined space starts after SMMU GR space, so IMPL_DEF page n
  * is page n + 2 in the SMMU register space.
  */
+#define ARM_SMMU_IMPL_DEF0	2
+#define ARM_SMMU_IMPL_DEF4	6
 #define ARM_SMMU_IMPL_DEF5	7
 
 #define ARM_SMMU_CB(s, n)	((s)->numpage + (n))
@@ -737,6 +734,7 @@ struct arm_smmu_device *arm_smmu_impl_init(struct arm_smmu_device *smmu);
 struct arm_smmu_device *nvidia_smmu_impl_init(struct arm_smmu_device *smmu);
 struct arm_smmu_device *qcom_smmu_impl_init(struct arm_smmu_device *smmu);
 struct arm_smmu_device *qsmmuv500_impl_init(struct arm_smmu_device *smmu);
+struct arm_smmu_device *qsmmuv2_impl_init(struct arm_smmu_device *smmu);
 struct arm_smmu_device *qcom_adreno_smmu_impl_init(struct arm_smmu_device *smmu);
 
 void arm_smmu_write_context_bank(struct arm_smmu_device *smmu, int idx);
