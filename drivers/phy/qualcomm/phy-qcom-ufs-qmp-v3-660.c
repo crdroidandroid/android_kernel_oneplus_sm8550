405a04f6625e35835d94015a3d41df447b8bb52c
kamasali Satyanarayan <quic_kamasali@quicinc.com>
2024-01-24 10:23:04 +0530

    phy: ufs: Remove the condition check to calibrate UFS Phy
    This patch removes the condition checks of major, minor
    and step to calibrate UFS Phy for phy-qcom-ufs-qmp-v3-660.ko
    driver.
    
    Change-Id: Ic6e91d185415f151cc90471577c83702a007f9af
    Signed-off-by: kamasali Satyanarayan <quic_kamasali@quicinc.com>

--- drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3-660.c
+++ drivers/phy/qualcomm/phy-qcom-ufs-qmp-v3-660.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2013-2018,2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "phy-qcom-ufs-qmp-v3-660.h"
@@ -16,27 +16,17 @@ int ufs_qcom_phy_qmp_v3_660_phy_calibrate(struct phy *generic_phy)
 	int err;
 	int tbl_size_A, tbl_size_B;
 	struct ufs_qcom_phy_calibration *tbl_A, *tbl_B;
-	u8 major = ufs_qcom_phy->host_ctrl_rev_major;
-	u16 minor = ufs_qcom_phy->host_ctrl_rev_minor;
-	u16 step = ufs_qcom_phy->host_ctrl_rev_step;
 
 	/* For UFS PHY's submode, 1 = G4, 0 = non-G4 */
 	is_g4 = !!ufs_qcom_phy->submode;
+
 	is_rate_B = (ufs_qcom_phy->mode == PHY_MODE_UFS_HS_B) ? true : false;
 
+	tbl_size_A = ARRAY_SIZE(phy_cal_table_rate_A_3_1_1);
 	tbl_size_B = ARRAY_SIZE(phy_cal_table_rate_B);
-	tbl_B = phy_cal_table_rate_B;
 
-	if ((major == 0x3) && (minor == 0x001) && (step >= 0x001)) {
-		tbl_A = phy_cal_table_rate_A_3_1_1;
-		tbl_size_A = ARRAY_SIZE(phy_cal_table_rate_A_3_1_1);
-	} else {
-		dev_err(ufs_qcom_phy->dev,
-			"%s: Unknown UFS-PHY version (major 0x%x minor 0x%x step 0x%x), no calibration values\n",
-			__func__, major, minor, step);
-		err = -ENODEV;
-		goto out;
-	}
+	tbl_A = phy_cal_table_rate_A_3_1_1;
+	tbl_B = phy_cal_table_rate_B;
 
 	err = ufs_qcom_phy_calibrate(ufs_qcom_phy,
 				     tbl_A, tbl_size_A,
@@ -48,7 +38,6 @@ int ufs_qcom_phy_qmp_v3_660_phy_calibrate(struct phy *generic_phy)
 			"%s: ufs_qcom_phy_calibrate() failed %d\n",
 			__func__, err);
 
-out:
 	return err;
 }
 
