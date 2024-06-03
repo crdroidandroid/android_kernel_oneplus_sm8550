48d198f9d941ee54c519fdbf4e34554897ec64e9
ShevT <evgeny.shishkov@gmail.com>
2024-06-03 12:05:37 +0300

    devpts: KernelSU: Fix: Failed to execute pm in terminal
    Change-Id: I2204e9a3939969385b5e6b127bbfa3c74a13f4db
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -602,6 +602,10 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 #if defined(CONFIG_KSU_SUSFS_SUS_SU)
 extern bool ksu_devpts_hook;
 extern int ksu_handle_devpts(struct inode*);
+#else
+#ifdef CONFIG_KSU
+extern int ksu_handle_devpts(struct inode*);
+#endif
 #endif
 
 /**
@@ -620,6 +624,10 @@ void *devpts_get_priv(struct dentry *dentry)
 		ksu_handle_devpts(dentry->d_inode);
 	}
 orig_flow:
+#else
+#ifdef CONFIG_KSU
+	ksu_handle_devpts(dentry->d_inode);
+#endif
 #endif
 
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
